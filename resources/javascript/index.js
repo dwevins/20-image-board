94417e6d89068d15517184c76798d73aba7f5c02
Danny Evins <dcastkid@gmail.com>
2016-08-12 15:46:05 -0500

    linter likes index now

--- resources/javascript/index.js
+++ resources/javascript/index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+export default function () {
+  const button = document.querySelector('.photos-header__button');
+  const form = document.querySelector('.form');
+  button.addEventListener('click', () => {
+    form.toggleClass('.hidden');
+  });
+}
--- tmp/babel-output_path-kL93cYMR.tmp/index.js
+++ tmp/babel-output_path-kL93cYMR.tmp/index.js
@@ -1,3 +1,11 @@
-define('index', ['exports'], function (exports) {
+define('index', ['exports', 'module'], function (exports, module) {
   'use strict';
+
+  module.exports = function () {
+    var button = document.querySelector('.photos-header__button');
+    var form = document.querySelector('.form');
+    button.addEventListener('click', function () {
+      form.toggleClass('.hidden');
+    });
+  };
 });
